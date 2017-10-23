c1d9ae0d3945f65ab237bd1891aea0fe90fc1afe
Solbrains <vuelara@gmail.com>
2017-10-23 20:00:26 +0300

    Database context
    Setup database context for entity framework core.

---
+++ DAL/Models/Purchase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    class Purchase
+    {
+        public int Id { get; set; }
+        //public DateTime DateOf { get; set; }
+    }
+}
---
+++ DAL/SolbrainsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class SolbrainsContext:DbContext
+    {
+        public SolbrainsContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
